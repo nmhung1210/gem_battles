f60993887d06e4852a2ec8b7059c372dd7d49b74
Hung Nguyen <nmhung1210@gmail.com>
2017-11-21 09:03:00 +0700

    + Adding logic checking

---
+++ src/screens/LevelSelectScreen.js
@@ -0,0 +1,100 @@
+import ui.View;
+import ui.ImageScaleView as ImageScaleView;
+import ui.widget.ButtonView as ButtonView;
+import ui.ScrollView as ScrollView;
+import ui.resource.Image as Image;
+import ui.widget.ButtonView as ButtonView;
+import src.common.define as DEF;
+
+var LevelPossitions = [
+    [86,1600],
+    [462,1520],
+    [160,1400],
+    [350,1157],
+    [354,894],
+    [52,728],
+    [450,532],
+    [169,359],
+    [334,206]
+];
+exports = Class(ui.View, function(supr) {
+    
+    this.init = function(opts)
+    {
+        supr(this, 'init', [opts]);
+        this._app = opts.app;                        
+        this._worldMapImage = new Image({url: "resources/images/ui/worldmap.jpg"});
+        this._worldMapImage.doOnLoad(initUI.bind(this));
+    };     
+
+    var initUI = function()
+    {
+        var screenWidth = this._app.screenWidth();
+        var screenHeight = this._app.screenHeight();
+        var imgW = this._worldMapImage.getWidth();
+        var imgH = this._worldMapImage.getHeight();
+
+        var scale = screenWidth / imgW;
+
+        var scrollWorldMapView = new ScrollView({
+            superview: this,
+            scrollBounds: {
+                minY: 0,
+                maxY: imgH * scale
+            },
+            scrollX: false,
+            x: 0,
+            y: 0,
+            width: screenWidth,
+            height: screenHeight
+        });
+        var imageWordMapView = new ImageScaleView({
+            superview: scrollWorldMapView,
+            image: this._worldMapImage,
+            scale:scale,
+            width: imgW,
+            height: imgH
+        });
+
+        scrollWorldMapView.scrollTo(0,imgH * scale-screenHeight,0);
+
+        for(var i=0; i < LevelPossitions.length; i++)
+        {
+            new LevelButton({
+                superview: imageWordMapView,
+                x: LevelPossitions[i][0],
+                y: LevelPossitions[i][1],
+                title: i+1,
+                on: {
+                    up: startLevel.bind(this)
+                }
+            });
+        }
+    }
+
+    var startLevel = function(level)
+    {
+        this.emit(DEF.EVENT_LEVEL_SELECTED,level);
+    }
+});
+
+var LevelButton = Class(ButtonView,function(supr){
+    this.init = function(opts)
+    {
+        opts = merge(opts,{
+            width: 96,
+            height: 96,
+            images: {
+                up: "resources/images/ui/button_0.png",
+                down: "resources/images/ui/button_1.png"
+            },
+            text: {
+                color: "#FAFAFA",
+                size: 64,
+                autoFontSize: false,
+                autoSize: false
+            }
+        });
+        supr(this, 'init', [opts]);
+    }
+});
---
+++ src/screens/LoadingScreen.js
@@ -0,0 +1,33 @@
+import ui.View as View;
+import ui.resource.loader as Loader;
+import ui.SpriteView as SpriteView;
+import src.common.define as DEF;
+
+exports = Class(View, function(supr) {
+    this.init = function(opts) {
+        supr(this, 'init', [opts]);
+        var app = opts.app;
+        this.loadingSprite  = new SpriteView({
+            superview: this,
+            x:app.screenWidth()/2-48,
+            y:app.screenHeight()/2-48,
+            width:96,
+            height:96,
+            url: "resources/images/ui/loading/loading",
+            frameRate: 30
+        });
+        this.loadingSprite.startAnimation('anim', {loop: true});
+        this.startLoader();
+    }
+
+    this.startLoader = function()
+    {
+        var mthis = this;
+        Loader.preload([
+            'resources/images/', 
+            'resources/sound'
+        ], function() {
+            mthis.emit(DEF.EVENT_LOADING_COMPLETE);
+        }); 
+    }
+});
